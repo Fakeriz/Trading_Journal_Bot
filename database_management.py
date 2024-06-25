2267a11c08e435c249b4ef04027a188d69370ca3
jigsaw1313 <mr.mhmdi93@gmail.com>
2024-06-26 00:20:34 +0330

    Update database structure and script

--- database_management.py
+++ database_management.py
@@ -2,21 +2,38 @@ import sqlite3
 
 # Initialize the database
 def init_db():
-    conn = sqlite3.connect('trades.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS trades
-                 (date DATETIME, time DATETIME, win_loss TEXT, side TEXT, rr INTEGER, pnl FLOAT, strategy TEXT, picture TEXT)''')
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('trades.db')
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS trades (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date DATETIME,
+                time DATETIME,
+                ticker TEXT,
+                win_loss TEXT,
+                side TEXT,
+                rr INTEGER,
+                pnl FLOAT,
+                strategy TEXT,
+                picture TEXT)
+        ''')
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
+        
 
-def save_trade(trade):
-    conn = sqlite3.connect('trades.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO trades (date, time, win_loss, side, rr, pnl, strategy, picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-              (trade['date'], trade['time'], trade['win_loss'], trade['side'], trade['rr'], trade['pnl'], trade['strategy'], trade['picture']))
-    conn.commit()
-    conn.close()
 
-# Call init_db when the script starts
-if __name__ == '__main__':
-    init_db()
+def save_trade(date, ticker, time, win_loss, side, rr, pnl, strategy, picture):
+    try:
+        conn = sqlite3.connect('trades.db')
+        c = conn.cursor()
+        c.execute('''
+            INSERT INTO trades (date, time,ticker, win_loss, side, rr, pnl, strategy, picture)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (date, time, ticker, win_loss, side, rr, pnl, strategy, picture))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
